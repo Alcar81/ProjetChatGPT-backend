98138db106964a90efc5d57e7ca7f8a81ebbc4ff
Alexandre <alexandre.carignan@gmail.com>
2024-10-18 08:09:37 -0400

    Initial commit

---
+++ migrations/20241018105341-create-user.js
@@ -0,0 +1,40 @@
+// backend/migrations/20241018105341-create-user.js
+
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      firstName: {
+        type: Sequelize.STRING
+      },
+      lastName: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING,
+        unique: true // Assurons-nous que l'email soit unique
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
---
+++ seeders/20241018120000-demo-user.js
@@ -0,0 +1,30 @@
+// backend/seeders/20241018120000-demo-user.js
+
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Users', [
+      {
+        firstName: 'Jean',
+        lastName: 'Dupont',
+        email: 'jean.dupont@example.com',
+        password: '$2b$10$N9qo8uLOickgx2f3o3aFe.OXn6UKO34gAcOt9DJOM1v3LQjN4whaK', // 'password' haché avec bcrypt
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        firstName: 'Marie',
+        lastName: 'Curie',
+        email: 'marie.curie@example.com',
+        password: '$2b$10$N9qo8uLOickgx2f3o3aFe.OXn6UKO34gAcOt9DJOM1v3LQjN4whaK', // 'password' haché avec bcrypt
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Users', null, {});
+  }
+};
